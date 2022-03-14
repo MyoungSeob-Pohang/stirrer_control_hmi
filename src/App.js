e684de1028da7cea1b0a1a9744535c1aff1d544a
Myoungseob <myoungseob91@gmail.com>
2022-03-14 18:14:27 +0900

    Project Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/Reset.css';
+import './css/App.css';
+import Header from './components/Header';
+import ReactFullpage from '@fullpage/react-fullpage';
+
+export const Fullpage = () => (
+    <ReactFullpage
+        //fullpage options
+        licenseKey={'YOUR_KEY_HERE'}
+        scrollingSpeed={1000}
+        render={({ state, fullpageApi }) => {
+            return (
+                <ReactFullpage.Wrapper>
+                    <div className="section">
+                        <p>Section 1</p>
+                        <button onClick={() => fullpageApi.moveSectionDown()}>Click me to move down</button>
+                    </div>
+                    <div className="section">
+                        <p>Section 2</p>
+                    </div>
+                    <div className="section">
+                        <p>Section 3</p>
+                    </div>
+                </ReactFullpage.Wrapper>
+            );
+        }}
+    />
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Fullpage />
+            <Header />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div className="Header">
+            <h2>Header Area</h2>
+        </div>
+    );
+}
+
+export default Header;
